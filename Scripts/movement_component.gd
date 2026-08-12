fcbfe32914d9e253e0e869f5e0a31df68b638cb1
jeelowo <plegariajamuel47@gmail.com>
2026-08-12 19:51:15 +0800

    player basic movement
    idle and walk animation

---
+++ Scripts/movement_component.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+@export var player = CharacterBody2D
+@export var player_stats: Node2D
+@export var animated_sprite: AnimatedSprite2D
+
+var direction : Vector2
+var idle_animations
+var current_animation
+var animation_finished = false
+
+func _ready() -> void:
+	idle_animations = ["Idle", "Idle_2"]
+	current_animation = idle_animations.pick_random()
+
+func _physics_process(_delta: float) -> void:
+	direction = (Input.get_vector 
+	("move_left", "move_right", "move_up", "move_down"))
+
+	player.velocity = direction * player_stats.movement_speed
+
+	if player.velocity == Vector2(0,0):
+		animated_sprite.play(current_animation)
+	else:
+		if Input.is_action_just_pressed("move_left"):
+			animated_sprite.flip_h = true
+		elif Input.is_action_just_pressed("move_right"):
+			animated_sprite.flip_h = false
+		animated_sprite.play("Walk")
+		current_animation = idle_animations.pick_random()
+
+func _on_animated_sprite_animation_finished() -> void:
+	if current_animation != idle_animations[0]:
+		current_animation = idle_animations[0]
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,11 @@
-extends Node2D
+extends CharacterBody2D
 
-@export var movement_speed := 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	add_to_group("Player")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	move_and_slide()
