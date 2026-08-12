9eae6ef3276926a2e02eab477511acb42acd6408
jeelowo <plegariajamuel47@gmail.com>
2026-08-12 21:24:43 +0800

    added Health Component
    used to heal and take damage

--- Scripts/health_component.gd
+++ Scripts/health_component.gd
@@ -1,6 +1,37 @@
 extends Node
 
-@onready var player : CharacterBody2D = get_tree().get_first_node_in_group("Player")
+#@onready var timer: Timer = $"../../Timer"
+@onready var player_stats: Node2D = $"../../Player Stats"
 
-func _process(_delta: float) -> void:
-	pass
+var current_hp : float
+
+signal damaged(amount: float)
+signal healed(amount: float)
+signal die
+
+func _ready() -> void:
+	print(player_stats.max_hp)
+	current_hp = player_stats.max_hp
+
+# Custom Functions
+func increase_max_hp(amount: float):
+	player_stats.max_hp += amount
+	current_hp += amount
+	print("max hp is now: " + str(player_stats.max_hp))
+
+func take_damage(amount: float):
+	current_hp = max(current_hp - amount, 0)
+	damaged.emit(amount)
+	print("current_hp: " + str(current_hp))
+
+	if current_hp <= 0:
+		print("you died lol")
+		die.emit()
+
+func heal(amount: float):
+	current_hp = min(current_hp + amount, player_stats.max_hp)
+	healed.emit(amount)
+	print("current_hp: " + str(current_hp))
+
+	if current_hp > player_stats.max_hp:
+		current_hp = player_stats.max_hp
--- Scripts/movement_component.gd
+++ Scripts/movement_component.gd
@@ -14,7 +14,7 @@ func _ready() -> void:
 	current_animation = idle_animations.pick_random()
 
 func _physics_process(_delta: float) -> void:
-	direction = (Input.get_vector 
+	direction = (Input.get_vector
 	("move_left", "move_right", "move_up", "move_down"))
 
 	player.velocity = direction * player_stats.movement_speed
