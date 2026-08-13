d08fd35e9c43cfa5b5ddd384514fa6a8200813f9
jeelowo <plegariajamuel47@gmail.com>
2026-08-13 20:43:04 +0800

    player invincibility frames

--- Scripts/Components/enemy_health_component.gd
+++ Scripts/Components/enemy_health_component.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var zombie_stats: Node2D = $"../../Zombie Stats"
 
 var current_hp : float
+var invincible : bool
 
 signal damaged(amount: float)
 signal healed(amount: float)
@@ -10,6 +11,7 @@ signal die
 
 func _ready() -> void:
 	current_hp = zombie_stats.max_hp
+	invincible = false
 
 #@onready var timer: Timer = $"../../Timer"
 # for testing
@@ -25,9 +27,10 @@ func increase_max_hp(amount: float):
 	print("max hp is now: " + str(zombie_stats.max_hp))
 
 func take_damage(amount: float):
-	current_hp = max(current_hp - amount, 0)
-	damaged.emit(amount)
-	print("current_hp: " + str(current_hp))
+	if not invincible:
+		current_hp = max(current_hp - amount, 0)
+		damaged.emit(amount)
+		print("current_hp: " + str(current_hp))
 
 	if current_hp <= 0:
 		print("you died lol")
--- Scripts/hitbox.gd
+++ Scripts/hitbox.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @onready var zombie_stats: Node2D = $"../../Zombie Stats"
 
+
+
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Player"):
 		var health = area.get_parent().get_node("Components/Health Component")
