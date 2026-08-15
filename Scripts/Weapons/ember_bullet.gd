ff773c96009d4b5154fa8240f6b0f7b810f1f70d
jeelowo <plegariajamuel47@gmail.com>
2026-08-15 20:59:43 +0800

    fixed: ember projectile targeting and rotation

--- Scripts/Weapons/ember_bullet.gd
+++ Scripts/Weapons/ember_bullet.gd
@@ -4,31 +4,29 @@ extends Area2D
 @onready var player: CharacterBody2D
 @onready var weapon_manager: Node2D = $"../../../"
 
-var direction : Vector2
-var random_direction : Vector2
-var current_target : Node2D
+var direction: Vector2
+var current_target: Node2D
+
 
 func _ready() -> void:
 	add_to_group("Projectile")
 
 	player = get_tree().get_first_node_in_group("Player")
 	global_position = player.global_position
+
+	# Random direction if there are no enemies to target
 	direction = Vector2.from_angle(randf() * TAU)
+
 
 func _process(delta: float) -> void:
 	weapon_manager.body_in_range = weapon_manager.body_in_range.filter(
 		is_instance_valid
 	)
 
-	if (!weapon_manager.body_in_range.is_empty() and 
-		current_target != weapon_manager.body_in_range[0]):
+	if not weapon_manager.body_in_range.is_empty() and \
+		current_target != weapon_manager.body_in_range[0]:
 
 		current_target = weapon_manager.body_in_range[0]
-
-		if !is_instance_valid(current_target):
-			weapon_manager.body_in_range.erase(current_target)
-			current_target = null
-			return
 
 		var target_health_component = current_target.get_node(
 			"Components/Health Component"
@@ -39,20 +37,22 @@ func _process(delta: float) -> void:
 			current_target = null
 			return
 
-		if weapon_manager.body_in_range.is_empty():
-			random_direction = Vector2(randf(), randf()).normalized()
-			direction = random_direction
-			print("no body in range")
+	if is_instance_valid(current_target):
+		direction = (
+			current_target.global_position - global_position
+		).normalized()
 
-		if !weapon_manager.body_in_range.is_empty():
-			direction = (current_target.global_position - global_position).normalized()
-
-		self.rotation = direction.angle()
-
+	# Always rotate and move toward the current direction
+	global_rotation = direction.angle()
 	global_position += ember_stats.proj_speed * delta * direction
+
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Zombie"):
 		var health = area.get_node("../../Components/Health Component")
-		health.take_damage(weapon_manager.roll_crit(ember_stats.damage, 
-		ember_stats.crit_dmg ,ember_stats.crit_chance))
+
+		health.take_damage(
+			weapon_manager.roll_crit(
+				ember_stats.damage,
+				ember_stats.crit_dmg,
+				ember_stats.crit_chance ) )
