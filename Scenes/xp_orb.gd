238f85f039d2a5e02e2e51b6cf196c8868ceb48a
jeelowo <plegariajamuel47@gmail.com>
2026-08-17 23:46:28 +0800

    feat: added roll_orb and xp_amount functions
    roll_orb - roll orb type using current chances
    xp_amount - randomizes xp gain based on orb type

--- Scenes/xp_orb.gd
+++ Scenes/xp_orb.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var xp_manager: Node2D = $".."
 
 var xp_value : int
-
+var orb_type : int
 
 func _ready() -> void:
 	pass
@@ -11,3 +11,5 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		xp_manager.current_player_xp += xp_value
+
+func 
--- Scripts/xp_manager.gd
+++ Scripts/xp_manager.gd
@@ -16,25 +16,48 @@ func modify_chance(orb_type: int):
 		0: # small 
 			medium_chance += 0.025
 			large_chance += 0.001
-
 			small_chance = 1 - (medium_chance + large_chance)
+
+			print("New Odds: \n Small: " + str(small_chance) 
+			+ "\nMedium:" +  str(medium_chance)
+			+ "\nLarge:" +  str(large_chance))
 
 		1: # medium
 			medium_chance -= 0.1
 			large_chance += 0.001
+			small_chance = 1 - (medium_chance + large_chance)
 
-			if medium_chance < 0.1:
-				medium_chance = 0.1
-
-				small_chance = 1 - (medium_chance + large_chance)
+			print("New Odds: \n Small: " + str(small_chance) 
+			+ "\nMedium:" +  str(medium_chance)
+			+ "\nLarge:" +  str(large_chance))
 
 		2: # large
 			medium_chance += 0.025
 			large_chance -= 0.08
-
-			if large_chance < 0.01:
-				large_chance = 0.01
-
 			small_chance = 1 - (medium_chance + large_chance)
+			
+			print("New Odds: \n Small: " + str(small_chance) 
+			+ "\nMedium:" +  str(medium_chance)
+			+ "\nLarge:" +  str(large_chance))
 		_:
 			print("WHAT????")
+
+func roll_orb() -> int:
+	var roll = randf()
+	if roll <= large_chance:
+		return 2
+	elif roll <= large_chance + medium_chance:
+		return 1
+	else:
+		return 0
+	return 0
+
+func xp_amount(orb_type : int) -> float:
+	match orb_type:
+		0:
+			return randi_range(90,110)
+		1: 
+			return randi_range(450,540)
+		2:
+			return randi_range(3300, 3850)
+	return 0
