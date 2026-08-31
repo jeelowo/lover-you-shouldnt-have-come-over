c9a9fb8bafee7ef90df1e9d9d64b23ac0eb022d0
jeelowo <plegariajamuel47@gmail.com>
2026-08-31 22:21:43 +0800

    feat: added upgrade panel UI template

--- Scenes/game_control.gd
+++ Scenes/game_control.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var healthbar: ProgressBar = $Healthbar
 @onready var player_health: Node2D = $"../../Player/Components/Health Component"
 @onready var player_stats: Node2D = $"../../Player/Player Stats"
+@onready var upgrade_panel: Panel = $"Upgrade Panel"
+
+func _ready() -> void:
+	upgrade_panel.visible = false
 
 func _process(delta: float) -> void:
 	if healthbar.value != player_health.current_hp:
